6f92f2444d0246d5819856899f31759b1208bd1d
Matt H <44037139+Mbwata@users.noreply.github.com>
2020-04-20 22:20:39 -0500

    Delete kubes_teardown.sh

--- kubes_teardown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-kubectl delete namespace jenkins
-kubectl delete namespace dieroll
-kubectl delete namespace sonarqube
-kubectl delete namespace nexus
-kubectl delete namespace ingress-nginx
