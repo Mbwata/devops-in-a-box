8d113aefb3fb7bd38e16fbcc31e609ead13e7596
Matt H <44037139+Mbwata@users.noreply.github.com>
2020-04-24 10:16:36 -0500

    fix sonarqube to be inside on node execution

--- build_jenkins_image.sh
+++ build_jenkins_image.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-docker rmi housems/diab_jenkins:halfbaked
+#docker rmi housems/diab_jenkins:halfbaked
 docker rmi housems/diab_jenkins:fullybaked
-docker build -t housems/diab_jenkins:halfbaked -f ./jenkins/Dockerfile.halfbaked ./jenkins
+#docker build -t housems/diab_jenkins:halfbaked -f ./jenkins/Dockerfile.halfbaked ./jenkins
 docker build -t housems/diab_jenkins:fullybaked -f ./jenkins/Dockerfile.fullybaked ./jenkins
-docker push housems/diab_jenkins:halfbaked
-docker push housems/diab_jenkins:fullybaked
+docker tag housems/diab_jenkins:fullybaked housems/diab_jenkins:latest 
+#docker push housems/diab_jenkins:halfbaked
+#docker push housems/diab_jenkins:fullybaked
+docker push housems/diab_jenkins
