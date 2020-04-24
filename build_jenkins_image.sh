3b9e1ab32f3f608268acaea87b2da553b872f1d4
Matt H <44037139+Mbwata@users.noreply.github.com>
2020-04-24 10:20:34 -0500

    Update build_jenkins_image.sh

--- build_jenkins_image.sh
+++ build_jenkins_image.sh
@@ -5,6 +5,4 @@ docker rmi housems/diab_jenkins:fullybaked
 #docker build -t housems/diab_jenkins:halfbaked -f ./jenkins/Dockerfile.halfbaked ./jenkins
 docker build -t housems/diab_jenkins:fullybaked -f ./jenkins/Dockerfile.fullybaked ./jenkins
 docker tag housems/diab_jenkins:fullybaked housems/diab_jenkins:latest 
-#docker push housems/diab_jenkins:halfbaked
-#docker push housems/diab_jenkins:fullybaked
 docker push housems/diab_jenkins
