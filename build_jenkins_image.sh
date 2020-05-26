a8fbd5a46c350b1c205802d5113241d52f90c07a
Matt H <housems@hotmail.com>
2020-05-26 17:48:37 -0500

    updates

--- build_jenkins_image.sh
+++ build_jenkins_image.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-docker build -t housems/diab_jenkins:halfbakedarm -f ./jenkins/Dockerfile.halfbaked ./jenkins
+#docker build -t housems/diab_jenkins:halfbakedarm -f ./jenkins/Dockerfile.halfbaked ./jenkins
 docker build -t housems/diab_jenkins:fullybakedarm -f ./jenkins/Dockerfile.fullybaked ./jenkins
 #docker tag housems/diab_jenkins:fullybaked housems/diab_jenkins:latest 
-docker push housems/diab_jenkins:halfbakedarm
+#docker push housems/diab_jenkins:halfbakedarm
 docker push housems/diab_jenkins:fullybakedarm
