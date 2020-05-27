00a469d55136db95b0fa7973c8e48b0cbc39e47a
Matt H <housems@hotmail.com>
2020-05-27 00:10:24 -0500

    lets try these

--- build_jenkins_image.sh
+++ build_jenkins_image.sh
@@ -2,7 +2,7 @@
 
 
 #docker build -t housems/diab_jenkins:halfbakedarm -f ./jenkins/Dockerfile.halfbaked ./jenkins
-docker build -t housems/diab_jenkins:fullybakedarm -f ./jenkins/Dockerfile.fullybaked ./jenkins
+docker build -t housems/diab_jenkins:fullybakedarm -f ./jenkins/Dockerfile.old ./jenkins
 #docker tag housems/diab_jenkins:fullybaked housems/diab_jenkins:latest 
 #docker push housems/diab_jenkins:halfbakedarm
 docker push housems/diab_jenkins:fullybakedarm
